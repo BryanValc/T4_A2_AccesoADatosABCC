95aac25f546c2b4196775ebfdac9b92fc549ec89
Bryan Valdez <bryan501th@gmail.com>
2021-04-22 17:01:28 -0500

    Se uso el codigo visto en clase

---
+++ src/vista/VentanaInicio.java
@@ -0,0 +1,35 @@
+package vista;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.security.AllPermission;
+
+import javax.swing.*;
+
+import controlador.AlumnoDAO;
+import modelo.Alumno;
+
+
+
+public class VentanaInicio {
+	
+	public static void main(String[] args) {
+		
+		//Suponiendo que los datos vienen desde la interfaz grafica
+		
+		/*Alumno a = new Alumno("01", "Luke", "Skywalker", "-", (byte)50, (byte)10, "ISC");
+		
+		AlumnoDAO aDAO = new AlumnoDAO();
+		
+		System.out.println(aDAO.insertarRegistro(a)?"EXITO":"Me cambio de carrera");*/
+		
+		
+		
+		
+		
+	}
+
+}
