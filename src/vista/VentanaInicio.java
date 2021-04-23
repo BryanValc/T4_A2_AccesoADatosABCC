e0de2e49a8b5a9d3da13d8e5bc2448baf7906260
Bryan Valdez <bryan501th@gmail.com>
2021-04-23 12:40:06 -0500

    Se reposicionaron algunos componentes y se empezo a usar combobox en algunos casos

--- src/vista/VentanaInicio.java
+++ src/vista/VentanaInicio.java
@@ -20,8 +20,9 @@ class Interfaz extends JFrame implements ActionListener{
 	JMenuItem menuItemAltas;
 	JInternalFrame recordAltas;
 	
+	JComboBox<String> comboEdad, comboCarrera, comboSemestre;
 	JButton agregar;
-	JTextField numControl, nombre, primerAp, segundoAp, edad, semestre, carrera;
+	JTextField numControl, nombre, primerAp, segundoAp;
 	
 	public Interfaz() {
 		getContentPane().setLayout(null);
@@ -63,57 +64,33 @@ class Interfaz extends JFrame implements ActionListener{
 		panelAltas.setBackground(Color.WHITE);
 		panelAltas.setBounds(1, 1, 567, 425);
 		
-		metodoMagico(new JLabel("Número de control"), panelAltas, 20, 20, 130, 20);
-		metodoMagico(new JLabel("Nombre"), panelAltas, 20, 40, 130, 20);
-		metodoMagico(new JLabel("Primer apellido"), panelAltas, 20, 60, 130, 20);
-		metodoMagico(new JLabel("Segundo apellido"), panelAltas, 20, 80, 130, 20);
-		metodoMagico(new JLabel("Edad"), panelAltas, 20, 100, 130, 20);
-		metodoMagico(new JLabel("Semestre"), panelAltas, 20, 120, 130, 20);
-		metodoMagico(new JLabel("Carrera"), panelAltas, 20, 140, 130, 20);
+		metodoMagico(new JLabel("NUMERO DE CONTROL:"), panelAltas, 90, 87, 130, 20);
+		metodoMagico(new JLabel("NOMBRES:"), panelAltas, 90, 110, 130, 20);
+		metodoMagico(new JLabel("APELLIDO PATERNO: "), panelAltas, 90, 133, 130, 20);
+		metodoMagico(new JLabel("APELLIDO MATERNO: "), panelAltas, 90, 156, 130, 20);
+		metodoMagico(new JLabel("EDAD: "), panelAltas, 90, 179, 130, 20);
+		metodoMagico(new JLabel("SEMESTRE: "), panelAltas, 90, 202, 130, 20);
+		metodoMagico(new JLabel("CARRERA: "), panelAltas, 90, 225, 130, 20);
 		
 		numControl = new JTextField();
 		nombre = new JTextField();
 		primerAp = new JTextField();
 		segundoAp = new JTextField();
-		edad = new JTextField();
-		semestre = new JTextField();
-		carrera = new JTextField();
+		comboEdad = new JComboBox<String>();
+		comboSemestre = new JComboBox<String>();
+		comboCarrera = new JComboBox<String>();
 		
-		metodoMagico(numControl, panelAltas, 130, 20, 100, 20);
-		metodoMagico(nombre, panelAltas, 130, 40, 100, 20);
-		metodoMagico(primerAp, panelAltas, 130, 60, 100, 20);
-		metodoMagico(segundoAp, panelAltas, 130, 80, 100, 20);
-		metodoMagico(edad, panelAltas, 130, 100, 50, 20);
-		metodoMagico(semestre, panelAltas, 130, 120, 50, 20);
-		metodoMagico(carrera, panelAltas, 130, 140, 100, 20);
+		metodoMagico(numControl, panelAltas, 206, 82, 134, 17);
+		metodoMagico(nombre, panelAltas, 130, 40, 100, 17);
+		metodoMagico(primerAp, panelAltas, 130, 60, 100, 17);
+		metodoMagico(segundoAp, panelAltas, 130, 80, 100, 17);
+		metodoMagico(comboEdad, panelAltas, 130, 100, 50, 17);
+		metodoMagico(comboSemestre, panelAltas, 130, 120, 50, 20);
+		metodoMagico(comboCarrera, panelAltas, 130, 140, 100, 20);
 		
 		agregar = new JButton("AGREGAR");
 		agregar.addActionListener(this);
 		metodoMagico(agregar, panelAltas, 130, 160, 100, 20);
-		
-		edad.addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent ke) {
-				String value = edad.getText();
-				int code=ke.getKeyCode();
-				if ((ke.getKeyChar()>='0' && ke.getKeyChar()<='9')|| (code==KeyEvent.VK_BACK_SPACE)) {
-					edad.setEditable(true);
-				}else{
-					edad.setEditable(false);
-				}
-			}
-		});
-		
-		semestre.addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent ke) {
-				String value = semestre.getText();
-				int code=ke.getKeyCode();
-				if ((ke.getKeyChar()>='0' && ke.getKeyChar()<='9')|| (code==KeyEvent.VK_BACK_SPACE)) {
-					semestre.setEditable(true);
-				}else{
-					semestre.setEditable(false);
-				}
-			}
-		});
 		
 		recordAltas.add(panelAltas);
 		dp.add(recordAltas);
